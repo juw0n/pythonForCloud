d459f786e6a3679c211d3ccc7d3d43bc5169dfbc
Oluwajuwon ODUNITAN <oduns07@gmail.com>
2023-05-15 19:23:46 +0100

    different way to import python module

--- advancePython/packageExample.py
+++ advancePython/packageExample.py
@@ -1,2 +1,15 @@
+# importing module from package
 import Greeting.Person.greet
 Greeting.Person.greet.sayHi()
+
+# importing packages as alias
+import Greeting.Person.greet as greet_user
+greet_user.sayHi()
+
+# importing module without package prefix
+from Greeting.Person import greet
+greet.sayHi()
+
+# importing only function from package module
+from Greeting.Person.greet import sayHi
+sayHi()
